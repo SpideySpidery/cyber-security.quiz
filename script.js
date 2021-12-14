064bfdbcbe086aa80f629ea0cc9f557d3648e574
Spideyspidery <75521850+Spideyspidery@users.noreply.github.com>
2021-12-14 13:10:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,386 @@
+const questions = [
+    {
+        question: "In which of the following, a person is constantly followed/chased by another person or group of several peoples ?",
+        optionA: "Phishing",
+        optionB: "Bulling",
+        optionC: "Stalking",
+        optionD: "Identity theft",
+        correctOption: "optionC"
+    },
+
+    {
+        question: " Which one of the following can be considered as the class of computer threats ?",
+        optionA: "Dos Attack",
+        optionB: "Phising",
+        optionC: "Binary 101 Virus",
+        optionD: "Hecker :|",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Which of the following is considered as the unsolicited commercial email ?",
+        optionA: "http://abc.mail.xyz",
+        optionB: "Yahoo",
+        optionC: "Proton Mail",
+        optionD: "Spam",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which of the following usually observe each activity on the internet of the victim, gather all information in the background, and send it to someone else ?",
+        optionA: "COVID 19",
+        optionB: "A Virus with eyes",
+        optionC: "Spyware",
+        optionD: "Cortona",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "_______ is a type of software designed to help the user's computer detect viruses and avoid them.",
+        optionA: "Savier of your Computer!",
+        optionB: "A hacker",
+        optionC: "js.html.css.php.com.io.net.org",
+        optionD: "AntiVirus",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which one of the following is a type of antivirus program ?",
+        optionA: "Quick heal",
+        optionB: "Mcafee",
+        optionC: "Kaspersky",
+        optionD: "All of the above",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "It can be a software program or a hardware device that filters all data packets coming through the internet, a network, etc. it is known as the_______:",
+        optionA: "AntiOxident",
+        optionB: "QuickVirus",
+        optionC: "Firewall",
+        optionD: "All of the above",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which of the following refers to stealing one's idea or invention of others and use it for their own benefits ?",
+        optionA: "Plagiarism",
+        optionB: "Show Answers",
+        optionC: "Flag/Report this question",
+        optionD: "None of the above",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Binary consists of which of the following numbers ?",
+        optionA: "1",
+        optionB: "Zero",
+        optionC: "One",
+        optionD: "All of the above",
+        correctOption: "optionD"
+    },
+
+    {
+        question: `Which of the following refers to exploring the appropriate, ethical behaviors related to the online environment and digital media platform ?`,
+        optionA: "Cyber Habits",
+        optionB: "OnlineInstructs.com",
+        optionC: "Cyberethics",
+        optionD: "Computer Rules",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which one of the following refers to the technique used for verifying the integrity of the message ?",
+        optionA: "Message Grits",
+        optionB: "InterGrits",
+        optionC: "Message Digest",
+        optionD: "MessageTech.xyz.com.net.io.org",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which one of the following usually used in the process of Wi-Fi-hacking ?",
+        optionA: "Aircrack-ng",
+        optionB: "WiFiHacks",
+        optionC: "Aircracking",
+        optionD: "Proton",
+        correctOption: "optionA"
+    },
+
+
+    {
+        question: "Which of the following is not a type of scanning ?",
+        optionA: "Xmas Tree Scan",
+        optionB: "Cloud Scan",
+        optionC: "Null Scan",
+        optionD: "SYN Stealth",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Which one of the following is actually considered as the first computer virus ?",
+        optionA: "aws.spidervirus.hk",
+        optionB: "Version666",
+        optionC: "Null:404",
+        optionD: "Creeper",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Code Red is a type of ________",
+        optionA: "A computer Virus",
+        optionB: "V1018 Version v.1.8",
+        optionC: "Firewall",
+        optionD: "Antivirus",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Which one of the following is also referred to as malicious software ?",
+        optionA: "Maliciousware",
+        optionB: "Ilegalware",
+        optionC: "Malware",
+        optionD: "Nurton",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Hackers usually used the computer virus for ______ purpose.",
+        optionA: "To log, monitor each and every user's stroke",
+        optionB: "To gain access the sensitive information like user's Id and Passwords",
+        optionC: "To corrupt the user's data stored in the computer system",
+        optionD: "All of the above",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "When was the first computer virus created ?",
+        optionA: "2009",
+        optionB: "1971",
+        optionC: "1970",
+        optionD: "2096",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which of the following is considered as the world's first antivirus program ?",
+        optionA: "Creeper",
+        optionB: "AntiVirus",
+        optionC: "MacAdFree",
+        optionD: "Reaper",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which type of the following malware does not replicate or clone them self's through infection ?",
+        optionA: "Trojans",
+        optionB: "Worms",
+        optionC: "Viruses",
+        optionD: "Insects",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Which of the following malware's type allows the attacker to access the administrative controls and enables his/or her to do almost anything he wants to do with the infected computers.",
+        optionA: "MOUSEs",
+        optionB: "TMs",
+        optionC: "@17#$",
+        optionD: "RATs",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which of the following is a type of independent malicious program that never required any host program ?",
+        optionA: "ZombiesTATs",
+        optionB: "Worm",
+        optionC: "VenusFlyTRAPtm",
+        optionD: "Virus:CODEred",
+        correctOption: "optionB"
+    },
+
+    {
+        question: " In order to ensure the security of the data/ information, we need to ____________ the data.",
+        optionA: "Password-Lock",
+        optionB: "Encrypt",
+        optionC: "FireWall",
+        optionD: "Delete/Remove",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Which of the following is considered as the first hacker's conference ?",
+        optionA: "ORSCOPE",
+        optionB: "HACKERConsole",
+        optionC: "DEFCON",
+        optionD: "OSCON",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which of the following known as the oldest phone hacking techniques used by hackers to make free calls?",
+        optionA: "Phishing",
+        optionB: "Cracking",
+        optionC: "Phreaking",
+        optionD: "Hacking",
+        correctOption: "optionC"
+    }
+
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+    while (shuffledQuestions.length <= 9) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1
+let playerScore = 0  
+let wrongAttempt = 0 
+let indexNumber = 0
+
+// function for displaying next question in the array to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+   
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer()
+    unCheckRadioButtons()
+    //delays next question displaying for a second
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 3) {
+        remark = "Bad Grades, Keep Practicing."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 4 && playerScore < 7) {
+        remark = "Average Grades, You can do better."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 7) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 10) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal and resets game
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
